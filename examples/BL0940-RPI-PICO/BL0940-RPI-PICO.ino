ba14acad90dd54e8aaf221b39bb898a92f59d0b9
Christopher Mendez <mcmchris@hotmail.com>
2023-02-18 11:12:25 -0400

    Library added

---
+++ examples/BL0940-RPI-PICO/BL0940-RPI-PICO.ino
@@ -0,0 +1,52 @@
+#include "MCM_BL0940.h"
+
+BL0940 bl0940;
+
+void setup() {
+  // For the USB, just use Serial as normal:
+  Serial.begin(115200);
+
+  // Configure Serial2 on pins TX=GP8 and RX=GP9
+  Serial2.begin(4800);
+
+  bl0940.Reset();
+  bl0940.setFrequency(60);    //50[Hz]
+  bl0940.setUpdateRate(800);  //400[ms]
+
+}
+
+void loop() {
+  float voltage;
+  bl0940.getVoltage(&voltage);
+  Serial.print(voltage,2);
+  Serial.println(" V");
+
+  float current;
+  bl0940.getCurrent(&current);
+  Serial.print(current,2);
+  Serial.println(" A");
+
+  float activePower;
+  bl0940.getActivePower(&activePower);
+  Serial.print(activePower,2);
+  Serial.println(" W");
+
+  float activeEnergy;
+  bl0940.getActiveEnergy(&activeEnergy);
+  Serial.print(activeEnergy,2);
+  Serial.println(" kWh");
+
+  float powerFactor;
+  bl0940.getPowerFactor(&powerFactor);
+  Serial.print(powerFactor);
+  Serial.println(" %");
+
+  float temperature;
+  bl0940.getTemperature(&temperature);
+  Serial.print(temperature,2);
+  Serial.println(" deg C");
+
+  Serial.println("");
+  delay(1000);
+}
+
